0bcac3e81327031bcbf86c35771d1f37504fcc5e
Gertinjo <gerticalaj2013@gmail.com>
2025-05-05 19:03:36 +0200

    last aprt OF PHP

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+echo "Hello";
+
+$a = 10;
+$b = 50;
+
+echo $a + $b;
+echo "<br>";
+$school = "Digital School";
+
+echo "I love $school";
+
+echo "<br>";
+
+echo str_word_count($school);
+echo "<br>";
+
+$programming = "Programing is not cool";
+
+echo str_replace("not" , "very" , $programming);
+
+echo "<br>";
+
+echo strlen($school);
+
+echo "<br>";
+
+echo strrev($school);
+
+
+
+?>
